01f81755d6f8c32eeedb297ffff31480fefe1462
JCKelley-CYBR <joshkelley@unomaha.edu>
2023-06-25 12:31:06 -0500

    Initial Commit - Integration V1.0

---
+++ Falcon-IdentityProtection.py
@@ -0,0 +1,150 @@
+register_module_line('Falcon-IdentityProtection', 'start', __line__())
+#######################
+# Description: CrowdStrike Falcon API Integration - CAG for XSOAR
+# Author: Joshua Kelley
+# Creation: June 2023
+#######################
+
+from falconpy import APIHarness
+import json
+import urllib3
+from datetime import datetime, timedelta, timezone
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+INTEGRATION_NAME='Falcon-Api-Integration-CAG'
+CLIENT_ID = demisto.params().get('credentials', {}).get('identifier') or demisto.params().get('client_id')
+CLIENT_SECRET = demisto.params().get('credentials', {}).get('password') or demisto.params().get('secret')
+
+# Functions
+
+######################
+# Description: Test connection to CrowdStrike Falcon API
+def test_module():
+    try:
+        falcon = APIHarness(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
+        response = falcon.command("GetSensorInstallersByQuery", limit=1)
+    except ValueError:
+        return 'Connection Error: The URL or The API key you entered is probably incorrect, please try again.'
+    return 'ok'
+
+######################
+# Description: Identity Protection API functions
+# functions: GetDetections - Get CrowdStrike Identity Protection Incidents
+#            IdentityProtection - Main function to call GetDetections and return XSOAR alerts
+# Parameters: falcon - Falcon API Harness
+# Returns: CrowdStrike API response and formatted XSOAR incident data (JSON)
+######################
+def GetDetections(falcon):
+    fetch_inc_interval = getFetchInterval()
+    GraphQL_Query = '''
+        {
+            incidents(createdAfter: "REPLACEME", severities: [LOW, MEDIUM, HIGH], first: 5, sortOrder: DESCENDING) {
+                edges {
+                    node {
+                        __typename
+                        incidentId
+                        severity
+                        type
+                        lifeCycleStage
+                        startTime
+                        endTime
+                        alertEvents {
+                            alertId
+                            state {
+                                lifeCycleStage
+                            }
+                            alertType
+                            timestamp
+                            eventType
+                            eventLabel
+                            eventSeverity
+                            startTime
+                            endTime
+                            resolved
+                            entities {
+                                type
+                                primaryDisplayName
+                                secondaryDisplayName
+                                hasADDomainAdminRole
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        '''
+
+    idp_query = GraphQL_Query.replace('REPLACEME', fetch_inc_interval)
+
+    variables = {
+        "string": "string, int, float"
+    }
+
+    BODY = {
+        "query": idp_query,
+        "variables": variables
+    }
+
+    response = falcon.command("api_preempt_proxy_post_graphql", body=BODY, filter="(severity:<0,severity:>=20)")
+    return response
+
+def IdentityProtection():
+    falcon = APIHarness(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
+    response = GetDetections(falcon)
+    response = response["body"]["data"]["incidents"]["edges"]
+    alert_return = []
+    if len(response) == 0:
+        return alert_return
+    for incident in response:
+        if len(incident) == 0:
+            return alert_return
+        if incident['node']['lifeCycleStage'] == 'NEW':
+            incident_data = json.dumps(incident, indent=4)
+            alert_return.append({
+                'name': "CS Identity Protection: " + incident['node']['type'],
+                'occurred': datetime.now(timezone.utc).astimezone().isoformat(),
+                'dbotMirrorId': incident['node']['incidentId'],
+                'rawJSON': incident_data
+                })
+
+
+    return alert_return
+
+####################
+# Description: This function will create an iso formatted datetime string offset by the incidentFetchInterval
+# Params: None
+# Return: datetime string for the fetch interval in UTC time and iso format
+# Example: 2023-06-14T15:25:00.000Z
+####################
+def getFetchInterval():
+    tz = timezone(timedelta(hours=0), name="UTC")
+    interval = demisto.params().get('incidentFetchInterval')
+    current_datetime = datetime.now(tz=tz) - timedelta(minutes=int(interval))
+    formatted_datetime = current_datetime.isoformat(timespec='milliseconds')
+    formatted_datetime = formatted_datetime.replace('+00:00', 'Z')
+    return formatted_datetime
+
+######################
+#    Main function   #
+######################
+def main():
+    command = demisto.command()
+    try:
+        if command == 'test-module':
+            result = test_module()
+            return_results(result)
+        elif command == 'fetch-incidents':
+            demisto.incidents(IdentityProtection())
+        elif command== 'identity-fetch-incidents':
+            return_results(IdentityProtection())
+        else:
+            raise NotImplementedError(f'CrowdStrike Falcon error: '
+                                      f'command {command} is not implemented')
+    except Exception as e:
+        return_error(str(e))
+    pass
+
+if __name__ in ('__main__', 'builtin', 'builtins'):
+    main()
+
+register_module_line('Falcon-IdentityProtection', 'end', __line__())
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# CrowdStrike-IdentityProtection-XSOAR
-Cortex XSOAR integration for CrowdStrike Falcon Identity Protection. Pull Incidents from the CS API into XSOAR
+# CrowdStrike Idenity Protection Integration for Cortex XSOAR (aka Demisto):
+Author: Joshua Kelley
+Version: 1.0.0
+
+# Overview:
+
+## Use Cases:
+- Pulling incidents from CrowdStrike Identity Protection API into Cortex XSOAR
+
+## Configuration Steps:
+1. Navigate to **Settings** > **Integrations** and click **BYOI**.
+2. Copy the content of [this file](FIXME) into the Integration coding area.
+3. Name the integration **Falcon-IdentityProtection**.
+   1. Select **Fetches incidents**.
+4. Create **two** Parameters:
+   1. Name **credentials**: 
+      1. Set **type** to Authentication and set to mandatory.
+      2. **Display username**: Client ID and set show to true.
+      3. **Display password**: Secret and set show to true.
+   2. Name **incidentFetchinterval**:
+      1. Set **type** to Short Text and set to mandatory.
+      2. Set **initial value** to 1. (Indicating 1 minute interval)
+      3. Set **Displayname** to "Interval to fetch incidents at in minutes.".
+      4. Set **Additional information** to "The Interval at which incidents will be fetched from CrowdStrike Identity Protection (Set this to be the same as the rest of XSOAR for fetching incidents, or you'll get duplicates)."
+5. Add **Command(s)**
+   1. `identity-fetch-incidents` - This command will allow you to manually fetch incident details and post them to the War Room or playground.
+   2. You do not need to add `test-module` as a command since its used by default with the test button in the integration settings, and `fetch-incidents` is used by default to fetch in the integration settings.
+6. Also, you will need to use a Docker image that has the **crowdstrike-falconpy** python module installed. 
+   1. You can use a command like this to build your own image: 
+   2. `/docker_image_create name={NAME HERE} base="demisto/python3-VERSION" dependencies="crowdstrike-falconpy"` 
+      1. (Replace {NAME HERE} with the name you want to give the image, and VERSION with the version of the demisto/python3 image you want to use as a base)
+
+## Other Requirements:
+You will need to also create a CrowdStrike API Client ID and Secret, and add them to the integration settings.
+
+### Incident Objects:
+The following incident objects need to be created in order for the integration to work properly:
+- **Incident Type**
+- **Layout**
+- **Mapper**
+- **Classifier**
+
+# TODO:
