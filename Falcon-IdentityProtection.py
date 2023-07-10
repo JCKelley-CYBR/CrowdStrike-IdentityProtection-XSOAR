81de6b05dd507e16abe53c2584c982188ce95a4f
JCKelley-CYBR <joshkelley@unomaha.edu>
2023-07-09 23:42:18 -0500

    Updated comments and added function to date status and send comment to CS IP

--- Falcon-IdentityProtection.py
+++ Falcon-IdentityProtection.py
@@ -4,6 +4,8 @@ register_module_line('Falcon-IdentityProtection', 'start', __line__())
 # Author: Joshua Kelley
 # Creation: June 2023
 #######################
+# V1 - 2023-06-15 - Initial Release
+# V1.5 - 2023-07-07 - Added function to update incident state in Identity Protection
 
 from falconpy import APIHarness
 import json
@@ -19,6 +21,9 @@ CLIENT_SECRET = demisto.params().get('credentials', {}).get('password') or demis
 
 ######################
 # Description: Test connection to CrowdStrike Falcon API
+# Parameters: None
+# Returns: Success or Failure message to Demisto
+######################
 def test_module():
     try:
         falcon = APIHarness(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
@@ -28,14 +33,26 @@ def test_module():
     return 'ok'
 
 ######################
-# Description: Identity Protection API functions
-# functions: GetDetections - Get CrowdStrike Identity Protection Incidents
-#            IdentityProtection - Main function to call GetDetections and return XSOAR alerts
+# Description: Identity Protection functions
+# functions: GetDetections - Get Identity Protection detections from CrowdStrike
+#            IdentityProtection - Main function to call GetDetections
+#            getFetchInterval - Get the current time minus the fetch interval
+#            identitySetIncident - Set the incident state in CrowdStrike Identity Protection, takes action and reason as parameters
+# Parameters:
+# Returns: CrowdStrike Identity Protection Incidents from the last fetch interval
+######################
+
+
+######################
+# Description: Query CrowdStrike Identity Protection for incidents matching GraphQL query
 # Parameters: falcon - Falcon API Harness
-# Returns: CrowdStrike API response and formatted XSOAR incident data (JSON)
+# Returns: CrowdStrike Identity Protection API response
 ######################
 def GetDetections(falcon):
     fetch_inc_interval = getFetchInterval()
+    # incident query can be updated to use updatedAfter instead of createdAfter in order 
+    # to capture incidents that have been updated, however this will also capture incidents
+    # that have been updated by the user and not just the system.
     GraphQL_Query = '''
         {
             incidents(createdAfter: "REPLACEME", severities: [LOW, MEDIUM, HIGH], first: 5, sortOrder: DESCENDING) {
@@ -111,6 +128,11 @@ def GetDetections(falcon):
     response = falcon.command("api_preempt_proxy_post_graphql", body=BODY, filter="(severity:<0,severity:>=20)")
     return response
 
+######################
+# Description: Retrieve Identity Protection Incidents from CrowdStrike
+# Parameters: None
+# Returns: Identity Protection Incidents from CrowdStrike and formatted for XSOAR
+######################
 def IdentityProtection():
     falcon = APIHarness(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
     response = GetDetections(falcon)
@@ -129,9 +151,48 @@ def IdentityProtection():
                 'dbotMirrorId': incident['node']['incidentId'],
                 'rawJSON': incident_data
                 })
-
-
     return alert_return
+
+######################
+# Description: Set the incident state in CrowdStrike Identity Protection
+# Parameters: action - action taken on the incident (CLOSED, IGNORED, etc)
+#             reason - reason for the action taken
+# Returns: CrowdStrike API response
+######################
+def identitySetIncident(action, reason, eventid):
+    # eventid = demisto.incidents()[0]['eventid']
+
+    falcon = APIHarness(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
+
+    GraphQL_Query = '''mutation {
+                        setIncidentState(
+                                input: {
+                                    lifeCycleStage: ACTION_TAKEN
+                                    incidentId: "NUMBER"
+                                    reason: "REASON"
+                                }
+                            ) {
+                                incident {
+                                    lifeCycleStage
+                                }
+                            }
+                        }
+    '''
+    idp_query_action = GraphQL_Query.replace('ACTION_TAKEN', action)
+    idp_query_number = idp_query_action.replace('NUMBER', eventid)
+    idp_query = idp_query_number.replace('REASON', reason)
+
+    variables = {
+        "string": "string, int, float"
+    }
+
+    BODY = {
+        "query": idp_query,
+        "variables": variables
+    }
+
+    response = falcon.command("api_preempt_proxy_post_graphql", body=BODY)
+    return response
 
 ####################
 # Description: This function will create an iso formatted datetime string offset by the incidentFetchInterval
@@ -148,7 +209,9 @@ def getFetchInterval():
     return formatted_datetime
 
 ######################
-#    Main function   #
+# Description: Main function
+# Parameters: None
+# Returns: Success or Failure message to Demisto console/playground
 ######################
 def main():
     command = demisto.command()
@@ -160,6 +223,8 @@ def main():
             demisto.incidents(IdentityProtection())
         elif command== 'identity-fetch-incidents':
             return_results(IdentityProtection())
+        elif command == 'Falcon-SetIncident':
+            return_results(identitySetIncident(demisto.args()['action'], demisto.args()['reason'], demisto.args()['eventid']))
         else:
             raise NotImplementedError(f'CrowdStrike Falcon error: '
                                       f'command {command} is not implemented')
