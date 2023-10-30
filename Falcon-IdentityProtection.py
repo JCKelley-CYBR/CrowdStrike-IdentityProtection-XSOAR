688bf41273c80b909b890581e5da92545575b2be
JCKelley-CYBR <joshkelley@unomaha.edu>
2023-10-30 10:58:43 -0500

    Updating Query and adding watchlist

--- Falcon-IdentityProtection.py
+++ Falcon-IdentityProtection.py
@@ -1,11 +1,14 @@
 register_module_line('Falcon-IdentityProtection', 'start', __line__())
 #######################
-# Description: CrowdStrike Falcon API Integration - for XSOAR
+# Description: CrowdStrike Falcon API Integration for XSOAR
 # Author: Joshua Kelley
 # Creation: June 2023
 #######################
 # V1 - 2023-06-15 - Initial Release
-# V1.5 - 2023-07-07 - Added function to update incident state in Identity Protection
+# V1.5 - 2023-06-16 - Added Identity Protection
+# V2 - 2023-06-23 - Fixed SpyBarron function to use QueryDevicesByFilterScroll and adhere to CS API limits
+# V3 - 2023-07-03 - Added Identity Protection Incident Set State function
+# V4 - 2023-10-04 - Added watchlist functions
 
 from falconpy import APIHarness
 import json
@@ -16,6 +19,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 INTEGRATION_NAME='Falcon-IdentityProtection'
 CLIENT_ID = demisto.params().get('credentials', {}).get('identifier') or demisto.params().get('client_id')
 CLIENT_SECRET = demisto.params().get('credentials', {}).get('password') or demisto.params().get('secret')
+# Update with your falcon instance us-1 or us-2
+INCIDENT_URL = "https://falcon.us-(1|2).crowdstrike.com/identity-protection/incidents/"
 
 # Functions
 
@@ -55,7 +60,7 @@ def GetDetections(falcon):
     # that have been updated by the user and not just the system.
     GraphQL_Query = '''
         {
-            incidents(createdAfter: "REPLACEME", severities: [LOW, MEDIUM, HIGH], first: 5, sortOrder: DESCENDING) {
+            incidents(updatedAfter: "REPLACEME", severities: [MEDIUM, HIGH], first: 5, sortOrder: DESCENDING) {
                 edges {
                     node {
                         __typename
@@ -78,33 +83,61 @@ def GetDetections(falcon):
                             eventSeverity
                             startTime
                             endTime
-                            userEntity {
-                                __typename
-                                primaryDisplayName
-                                secondaryDisplayName
-                                emailAddresses
-                                mostRecentActivity
-                                accounts {
-                                    __typename
-                                    enabled
-                                    description
-                                    dataSource
-                                }
-                            }
-                            endpointEntity {
-                                __typename
-                                primaryDisplayName
-                                hostName
-                                mostRecentActivity
-                                agentId
-                                lastIpAddress
-                                operatingSystemInfo {
-                                    __typename
-                                    displayName
-                                    version
-                                    servicePack
-                                    target
-                                    name
+                            relatedEvents(first: 10) {
+                                nodes {
+                                    eventType
+                                    timestamp
+                                    eventId
+                                    ... on TimelineUserOnEndpointActivityEvent {
+                                        userEntity {
+                                            __typename
+                                            primaryDisplayName
+                                            secondaryDisplayName
+                                            emailAddresses
+                                            mostRecentActivity
+                                            accounts {
+                                                __typename
+                                                enabled
+                                                description
+                                                dataSource
+                                            }
+                                        }
+                                        endpointEntity {
+                                            __typename
+                                            primaryDisplayName
+                                            hostName
+                                            mostRecentActivity
+                                            agentId
+                                            lastIpAddress
+                                            staticIpAddresses
+                                            operatingSystemInfo {
+                                                __typename
+                                                displayName
+                                                version
+                                                servicePack
+                                                target
+                                                name
+                                            }
+                                        }
+                                        targetEntity {
+                                            primaryDisplayName
+                                            ... on EndpointEntity {
+                                                hostName
+                                                mostRecentActivity
+                                                agentId
+                                                lastIpAddress
+                                                staticIpAddresses
+                                                operatingSystemInfo {
+                                                    __typename
+                                                    displayName
+                                                    version
+                                                    servicePack
+                                                    target
+                                                    name
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -209,6 +242,116 @@ def getFetchInterval():
     return formatted_datetime
 
 ######################
+# Description: Set the watchlist value for a user in CrowdStrike Identity Protection to True
+# Parameters: id - samAccountName for the user to add to the watchlist
+# Returns: CrowdStrike API response
+######################
+def identityAddWatchList(falcon, id):
+    GraphQL_Query = '''mutation watchSomeUser {
+        addEntitiesToWatchList(input: { 
+            entityQuery: {
+                    samAccountNames: "REPLACEME"
+                }
+            })
+            {
+                updatedEntities
+                {
+                    primaryDisplayName
+                    secondaryDisplayName
+                    watched
+                }
+                failures
+                {
+                    entityIds
+                    errorDetails
+                    {
+                        message
+                    }
+                }
+            }
+    }'''
+
+    idp_query = GraphQL_Query.replace('REPLACEME', id)
+
+    variables = {
+        "string": "string, int, float"
+    }
+
+    BODY = {
+        "query": idp_query,
+        "variables": variables
+    }
+
+    response = falcon.command("api_preempt_proxy_post_graphql", body=BODY)
+    return response
+
+######################
+# Description: Set the watchlist value for a user in CrowdStrike Identity Protection to False
+# Parameters: id - samAccountName for the user to remove from the watchlist
+# Returns: CrowdStrike API response
+######################
+# Requies the DOMAINHERE to be updated with your own domain name(s)
+def identityRemoveWatchList(falcon, id):
+    GraphQL_Query = '''mutation unwatchSomeUser {
+        removeEntitiesFromWatchList(input: { 
+            entityQuery: {
+                    secondaryDisplayNames: "DOMAINHERE\\\\REPLACEME"
+                }
+            })
+            {
+                updatedEntities
+                {
+                    primaryDisplayName
+                    secondaryDisplayName
+                    watched
+                }
+                failures
+                {
+                    entityIds
+                    errorDetails
+                    {
+                        message
+                    }
+                }
+            }
+    }'''
+
+    idp_query = GraphQL_Query.replace('REPLACEME', id)
+
+    variables = {
+        "string": "string, int, float"
+    }
+
+    BODY = {
+        "query": idp_query,
+        "variables": variables
+    }
+
+    response = falcon.command("api_preempt_proxy_post_graphql", body=BODY)
+    return response
+
+######################
+# Description: Add a list of users to the watchlist in CrowdStrike Identity Protection - Loops through a list of samAccountNames
+# Parameters: ids - list of samAccountNames to add to the watchlist
+# Returns: Success or Failure message
+######################
+def watchList(action, ids):
+    falcon = APIHarness(client_id=CLIENT_ID,client_secret=CLIENT_SECRET)
+    status = 'Success'
+    ids = ids.split(',')
+    for id in ids:
+        if action == "add":
+            response = identityAddWatchList(falcon, id)
+        elif action == "remove":
+            response = identityRemoveWatchList(falcon, id)
+        else:
+            return 'Failure, invalid action'
+        print(response['body'])
+        if response['status_code'] != 200:
+            status = 'Failure'
+    return status
+
+######################
 # Description: Main function
 # Parameters: None
 # Returns: Success or Failure message to Demisto console/playground
@@ -225,6 +368,10 @@ def main():
             return_results(IdentityProtection())
         elif command == 'Falcon-SetIncident':
             return_results(identitySetIncident(demisto.args()['action'], demisto.args()['reason'], demisto.args()['eventid']))
+        elif command == 'Falcon-AddWatchList':
+            return_results(watchList("add", demisto.args()['id']))
+        elif command == 'Falcon-RemoveWatchList':
+            return_results(watchList("remove", demisto.args()['id']))
         else:
             raise NotImplementedError(f'CrowdStrike Falcon error: '
                                       f'command {command} is not implemented')
